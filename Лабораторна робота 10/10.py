ba3c0167ad7d724e9f7c2fa62c6dcb505f0c889f
h3cks <161031571+h3cks@users.noreply.github.com>
2024-04-24 10:36:42 +0300

    Update 10.py

--- Лабораторна робота 10/10.py
+++ Лабораторна робота 10/10.py
@@ -21,10 +21,11 @@ def write_answer(file_path, surname, answer, next_question):
 file_path = "questions_and_answers.txt"
 
 # Перший студент записує питання
-write_question(file_path, "Іванов", "Як створити список у Python?")
+write_question(file_path, "Сущенко", "Як створити список у Python?")
 
 # Другий студент записує відповідь та питання для наступного
-write_answer(file_path, "Петров", "Для створення списку у Python використовується квадратні дужки [], наприклад: my_list = [1, 2, 3].", "Як видалити елемент зі списку у Python?")
+write_answer(file_path, "???", "???","???")
 
 # Посилання на файл для завантаження
 print("Файл з питаннями та відповідями доступний за посиланням: <посилання>")
+
